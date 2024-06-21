0aa7da42af79f22d0c39b95169291ba2f9c8d30f
DianYueGuo <dianyueguo@gmail.com>
2024-06-21 14:19:48 +0800

    Add Game class with basic initialization
    - Created game.py file to define the Game class.
    - Implemented __init__ method to initialize width and height attributes.
    - Added placeholder run_cycle_once method.
    - Included a main block for future execution logic.

---
+++ src/game.py
@@ -0,0 +1,12 @@
+class Game:
+    def __init__(self, width=50, height=50):
+        self._width = width
+        self._height = height
+        
+        pass
+
+    def run_cycle_onece():
+        pass
+
+if __name__ == "__main__":
+    pass
